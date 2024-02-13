11b4d90082c5fc6ae60ad308a40065f2359b9a80
MigueDevp <angelrr.ti22@utsjr.edu.mx>
2024-02-13 14:53:14 -0600

    MI NOMBRE

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main>
         <div className={styles.container}>
+          <p>MIGUEL ANGEL RAMIREZ RAMIREZ</p>
           <nav className={styles.navbar}>
             <a href="#">HOME</a>
             <a href="#">ACERCA</a>
