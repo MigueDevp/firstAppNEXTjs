1c6cc509630a1437a1427aedbdbf6246f8f655d9
MigueDevp <angelrr.ti22@utsjr.edu.mx>
2024-02-16 15:30:20 -0600

    APLICANDO TAILWINDCSS

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
 import styles from "@/styles/index.module.css";
 
+
 export default function Home() {
   return (
     <main>
-        <div className={styles.container}>
-          <p>MIGUEL ANGEL RAMIREZ RAMIREZ</p>
-          <nav className={styles.navbar}>
-            <a href="#">HOME</a>
-            <a href="#">ACERCA</a>
-          </nav>
+      <nav className="bg-blue-600 p-4">
+      <div className="container mx-auto flex items-center justify-end">
+        <div className="space-x-4">
+          <a href="#" className="text-white hover:text-gray-700">HOME</a>
+          <a href="#" className="text-white hover:text-gray-700">SOBRE MI</a>
         </div>
-    </main>
+      </div>
+    </nav>
+    <div className="bg-red-500 p-6 text-white text-center">
+        <h2 className="text-2xl font-bold">MIGUEL ANGEL RAMIREZ RAMIREZ</h2>
+        <p>Usando TAILWINDCSS</p>
+      </div>
+   </main> 
   );
-  
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{html,js,tsx}',
+    './pages/**/*.{html,js}',
+    './index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
