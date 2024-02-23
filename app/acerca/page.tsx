e698e88b98749eafd16d462f8bbacb5aad59e55c
MigueDevp <angelrr.ti22@utsjr.edu.mx>
2024-02-23 14:44:30 -0600

    MAS CAMBIOS DE MARGIN Y PADDING

--- app/acerca/page.tsx
+++ app/acerca/page.tsx
@@ -1,3 +1,10 @@
 export default function Page(){
-    return <p> Pagina Acerca de  </p>
+    return (
+        <div className="bg-green-500">
+            <div className="bg-green-300">
+                <p>HOLA ESTA ES MI DESCRIPCION DE LA PAGINA DE ABOUT</p>
+            </div>
+        </div>
+    )
 }
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import styles from "@/styles/index.module.css";
 import Navegation from "./components/Navigation";
-
 export default function Home() {
   return (
     <main>
@@ -9,10 +8,9 @@ export default function Home() {
         <p>Usando TAILWINDCSS</p>
       </div>
         <div className="bg-lime-600 p-6 text-white flex flex-col items-center">
-          <img src="/images/pexels-pixabay-163064.jpg" alt="IMAGEN GENERICA" className="w-96 h-400 object-cover" />
+          <img src="/images/pexels-pixabay-163064.jpg" alt="IMAGEN GENERICA" className="my-4 w-96 h-400 object-cover" />
           <p className="my-3">IMAGEN GENERICA</p>
         </div>
-      
     </main>
   );
 }
