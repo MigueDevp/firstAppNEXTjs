acad5a90fa753b390101eb0ea286a899ef683319
MigueDevp <angelrr.ti22@utsjr.edu.mx>
2024-02-23 14:34:18 -0600

    LINK EN VEZ DE ETIQUETA a Y USO DEL COMPONENTE IMG

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,18 +1,36 @@
-const Navegation = () => {
-    return (
-         <nav className="bg-blue-600 p-4">
-      <div className="container mx-auto flex items-center justify-center">
-        <div className="space-x-4">
-          <a href="./" className="text-white hover:text-gray-700">HOME</a>
-          <a href="./acerca" className="text-white hover:text-gray-700">ABOUT</a>
-          <a href="./documentation" className="text-white hover:text-gray-700">DOCUMENTATION</a>
-          <a href="./experiences" className="text-white hover:text-gray-700">EXPERIENCES</a>
-          <a href="./buy" className="text-white hover:text-gray-700">BUY</a>
-        </div>
-      </div>
+import Link from 'next/link'
+export default function Navegation() {
+  return (
+    <nav className="bg-blue-600 p-4">
+       <div className="container mx-auto flex items-center justify-center">
+          <div className="space-x-4">
+              <Link href="/" className='text-white hover:text-gray-700'>HOME</Link>
+              <Link href="./acerca" className='text-white hover:text-gray-700'>ACERCA</Link>
+              <Link href="#" className='text-white hover:text-gray-700'>DOCUMENTATION</Link>
+              <Link href="#" className='text-white hover:text-gray-700'>BUY</Link>
+       </div>
+           </div>
     </nav> 
-    );
+  )
+      
 }
+// const Navegation = () => {
+//     return (
+//     <nav className="bg-blue-600 p-4">
+//       <div className="container mx-auto flex items-center justify-center">
+//         <div className="space-x-4">
+//           <a href="./" className="text-white hover:text-gray-700">HOME</a>
+          
+//           <a href="./acerca" className="text-white hover:text-gray-700">ABOUT</a>
+//           <a href="#" className="text-white hover:text-gray-700">DOCUMENTATION</a>
+//           <a href="#" className="text-white hover:text-gray-700">EXPERIENCES</a>
+//           <a href="#" className="text-white hover:text-gray-700">BUY</a>
+//         </div>
+//       </div>
+//     </nav> 
+//     );
+
+// }
 
 
-export default Navegation;
+// export default Navegation;
--- app/page.tsx
+++ app/page.tsx
@@ -4,24 +4,15 @@ import Navegation from "./components/Navigation";
 export default function Home() {
   return (
     <main>
-      
-    <div className="bg-red-500 p-6 text-white text-center">
+      <div className="bg-red-500 p-6 text-white text-center">
         <h2 className="text-2xl font-bold">MIGUEL ANGEL RAMIREZ RAMIREZ</h2>
         <p>Usando TAILWINDCSS</p>
       </div>
-      <div className="bg-slate-500 w-600 h-600 text-center text-white">
-        <p>Hola, probando probando...</p>
-
-      </div>
-   </main> 
+        <div className="bg-lime-600 p-6 text-white flex flex-col items-center">
+          <img src="/images/pexels-pixabay-163064.jpg" alt="IMAGEN GENERICA" className="w-96 h-400 object-cover" />
+          <p className="my-3">IMAGEN GENERICA</p>
+        </div>
+      
+    </main>
   );
 }
-
-
-
-
-
-
-
-
-
